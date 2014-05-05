a8ddf3b090569cd1d494ec4613547064648d902d
marha915 <m.harnwall@gmail.com>
2014-05-05 16:16:47 +0200

    Revert 6875ace..c28cc07
    This rolls back to commit 6875ace87eaff78cf7c8d4802b05ed018e237696.

--- browseImage.m
+++
@@ -1,9 +0,0 @@
-%%% Function: browseImage
-%%% Input: None
-%%% Output: whole file path
-
-function [filePath] = browseImage()
-
-[file path] = uigetfile({'*.jpg';'*.png'}, 'Pick a file');
-
-filePath = [path file];
